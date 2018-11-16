b4904ac8f233b2f363c5bbb0e3363156bb190804
Unknown <Klaus.Pruenster@gmail.com>
2018-11-16 22:00:14 +0100

    added DAL project

---
+++ uFood.Infrastructure.OpenDataHub/Model/Gastronomies.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace uFood.Infrastructure.OpenDataHub.Model
+{
+	public class Gastronomies
+	{
+		public IEnumerable<Gastronomy> GastronomyList { get; set; } = new List<Gastronomy>();
+	}
+}
