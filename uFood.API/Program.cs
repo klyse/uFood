02ff0aaf68afda5950210d383e2d4fee1e2cf860
Unknown <Klaus.Pruenster@gmail.com>
2018-11-16 19:15:05 +0100

    changed folder names

--- uFood.API/Program.cs
+++ uFood.API/Program.cs
@@ -12,6 +12,6 @@ namespace uFood.API
 
 		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
 			WebHost.CreateDefaultBuilder(args)
-				.UseStartup<Startup>();
+				   .UseStartup<Startup>();
 	}
 }
--- uFood.Data/DataRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace uFood.Data
-{
-	public class DataRepository
-	{
-		public IEnumerable<string> LocalRestaurants { get; set; }
-	}
-}
