335877dd3f736981093ff0e246c253fc5b2d97c1
Unknown <Klaus.Pruenster@gmail.com>
2018-11-17 03:05:03 +0100

    added user model

---
+++ uFood.Infrastructure/Models/User/User.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using uFood.Infrastructure.Models.Food;
+
+namespace uFood.Infrastructure.Models.User
+{
+	public class User
+	{
+		[BsonId]
+		public ObjectId ID { get; set; }
+
+		public string Name { get; set; }
+
+		public IEnumerable<Intolerance.Intolerance> Intolerances { get; set; }
+
+		public IEnumerable<Dish> History { get; set; }
+	}
+}
